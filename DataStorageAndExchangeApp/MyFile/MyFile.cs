50319465f26bbe9d0f630681bd5b39a67bcdac25
kireevkonstantin <kireevkonstantin11@gmail.com>
2021-07-03 11:57:35 +0400

    Deleted PLWPF; Updated File and User classses

--- DataStorageAndExchangeApp/MyFile/MyFile.cs
+++ DataStorageAndExchangeApp/MyFile/MyFile.cs
@@ -8,5 +8,49 @@ namespace MyFile
 {
     class MyFile
     {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public byte[] data { get; set; }
+        public string Description { get; set; }
+        public DateTime AddingDate { get; private set; }
+        public int CreatorID { get; set; }
+        public DateTime LastModified { get; private set; }
+        public int TimesDowloaded { get; private set; }
+
+        public MyFile(int id, string fileName, byte[] data, string description, DateTime addingDate,
+            int creatorID, DateTime lastModified, int timesDowloaded)
+        {
+            Id = id;
+            FileName = fileName;
+            this.data = data;
+            Description = description;
+            AddingDate = addingDate;
+            CreatorID = creatorID;
+            LastModified = lastModified;
+            TimesDowloaded = timesDowloaded;
+        }
+
+        public MyFile(int id, string fileName, string description, DateTime addingDate, 
+            int creatorID, DateTime lastModified, int timesDowloaded)
+        {
+            Id = id;
+            FileName = fileName;
+            Description = description;
+            AddingDate = addingDate;
+            CreatorID = creatorID;
+            LastModified = lastModified;
+            TimesDowloaded = timesDowloaded;
+        }
+
+        public MyFile(int id, string fileName, byte[] data, string description, int creatorID)
+        {
+            Id = id;
+            FileName = fileName;
+            this.data = data;
+            Description = description;
+            CreatorID = creatorID;
+        }
+
+        public MyFile() { }
     }
 }
--- DataStorageAndExchangeApp/User/User.cs
+++ DataStorageAndExchangeApp/User/User.cs
@@ -8,5 +8,69 @@ namespace User
 {
     public class User
     {
+        public int ID { get; set; }
+        public string Login { get; private set; }
+        public string  Nickname { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string UserDescription { get; set; }
+        public int FilesAdded { get; private set; }
+        public int FilesDownloaded { get; private set; }
+        public DateTime RegistrationDate { get; private set; }
+        public int RoleID { get; set; }
+
+        public User(int iD, string login, string nickname, string firstName, 
+            string lastName, string password, string userDescription, 
+            int filesAdded, int filesDownloaded, DateTime registrationDate, int roleID)
+        {
+            ID = iD;
+            Login = login;
+            Nickname = nickname;
+            FirstName = firstName;
+            LastName = lastName;
+            Password = password;
+            UserDescription = userDescription;
+            FilesAdded = filesAdded;
+            FilesDownloaded = filesDownloaded;
+            RegistrationDate = registrationDate;
+            RoleID = roleID;
+        }
+
+        public User(int iD, string nickname, string firstName, string lastName,
+            string password, string userDescription, int filesAdded, 
+            int filesDownloaded, DateTime registrationDate, int roleID)
+        {
+            ID = iD;
+            Nickname = nickname;
+            FirstName = firstName;
+            LastName = lastName;
+            Password = password;
+            UserDescription = userDescription;
+            FilesAdded = filesAdded;
+            FilesDownloaded = filesDownloaded;
+            RegistrationDate = registrationDate;
+            RoleID = roleID;
+        }
+
+        public User(int iD, string login, string nickname, string firstName, 
+            string lastName, string password, string userDescription)
+        {
+            ID = iD;
+            Login = login;
+            Nickname = nickname;
+            FirstName = firstName;
+            LastName = lastName;
+            Password = password;
+            UserDescription = userDescription;
+        }
+
+        public User(int iD, string nickname)
+        {
+            ID = iD;
+            Nickname = nickname;
+        }
+
+        public User() { }
     }
 }
