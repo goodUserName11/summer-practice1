2252f6b2aa782046ea537a96bf078e95a4483a2c
kireevkonstantin <kireevkonstantin11@gmail.com>
2021-07-05 19:44:48 +0400

    Created full data base script; Changed other scripts location

--- DataBase scripts/Other scripts/CreateChangeFileNameDescProc.sql
+++ DataBase scripts/Other scripts/CreateChangeFileNameDescProc.sql
@@ -1,7 +1,7 @@
 USE [DataStotageAndExchangeDB]
 GO
 
-/****** Object:  StoredProcedure [dbo].[ChangeFileNameDesc]    Script Date: 05.07.2021 13:53:53 ******/
+/****** Object:  StoredProcedure [dbo].[ChangeFileNameDesc]    Script Date: 05.07.2021 19:42:16 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -11,6 +11,7 @@ GO
 CREATE PROCEDURE [dbo].[ChangeFileNameDesc] 
 	-- Add the parameters for the stored procedure here
 	@fileId int,
+	@userId int,
 	@fileName nvarchar(20),
 	@description nvarchar(200)
 AS
@@ -18,12 +19,16 @@ BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
+	DECLARE @roleId int
+    -- Insert statements for procedure here
+	SELECT @roleId = RoleID FROM Users
+	WHERE ID = @userId
 
     -- Insert statements for procedure here
 	UPDATE Files
 	SET FileName = @fileName,
 		FileDescription = @description
-	WHERE ID = @fileId
+	WHERE ID = @fileId AND (CreatorID = @userId OR @roleId = 1)
 END
 GO
 
